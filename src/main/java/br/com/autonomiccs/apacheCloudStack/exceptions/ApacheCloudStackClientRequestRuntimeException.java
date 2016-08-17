eecbb10c6acbd544d87df0697b21f98a44246393
Rafael Weingärtner <rafaelweingartner@gmail.com>
2016-08-17 16:36:12 -0300

    Prepare project to be closed and released to maven central repo

--- src/main/java/br/com/autonomiccs/apacheCloudStack/exceptions/ApacheCloudStackClientRequestRuntimeException.java
+++ src/main/java/br/com/autonomiccs/apacheCloudStack/exceptions/ApacheCloudStackClientRequestRuntimeException.java
@@ -19,7 +19,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package br.com.autonomiccs.apacheCloudStack;
+package br.com.autonomiccs.apacheCloudStack.exceptions;
 
 import org.apache.http.HttpStatus;
 
